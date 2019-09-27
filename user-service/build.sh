37e5bcc92044c0717cee455475a14a4bc96c6328
张志伟 <zhiwei.zhang@hand-china.com>
2019-09-27 11:24:39 +0800

    [MOD]修改

--- user-service/build.sh
+++ user-service/build.sh
@@ -4,4 +4,4 @@ mvn package
 
 docker build -t user-service:latest .
 
-docker run -d -p 8081:8081 --link mysql:mysql_host --link --name userservice user-service:latest
+docker run -d -p 8081:8081 --link mysql:mysql_host --name userservice user-service:latest
